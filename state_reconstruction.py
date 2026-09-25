0296a2d64858af931988a7ed68d0ab19311f8c4d
Sohail Hussain <sohail.hussain072002@gmail.com>
2026-09-25 21:28:41 +0530

    Add line based state reconstruction

--- state_reconstruction.py
+++ state_reconstruction.py
@@ -33,3 +33,28 @@ def reconstruct_state(database_path, target_state_index):
         current_state[variable_name] = value
 
     return current_state
+
+
+def reconstruct_state_at_line(database_path, target_line_number):
+    connection = sqlite3.connect(database_path)
+
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        SELECT line_number, variable_name, serialized_value
+        FROM variable_states
+        WHERE line_number <= ?
+        ORDER BY id ASC
+    """, (target_line_number,))
+
+    rows = cursor.fetchall()
+
+    connection.close()
+
+    current_state = {}
+
+    for line_number, variable_name, serialized_value in rows:
+        value = pickle.loads(serialized_value)
+        current_state[variable_name] = value
+
+    return current_state
